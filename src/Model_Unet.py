f5638a589acff16f679d3cdb8bbc9a47eac5833b
AlexTintin <alexis.benamira@institutoptique.fr>
2019-10-31 14:56:38 +0100

    update Unet

--- src/Model_Unet.py
+++ src/Model_Unet.py
@@ -1,14 +1,6 @@
 import torch.nn as nn
 import torch
 
-class Flatten(nn.Module):
-    def forward(self, input):
-        return input.view(input.size(0), -1)
-
-class UnFlatten(nn.Module):
-    def forward(self, input, size=1024):
-        return input.view(input.size(0), 16, 280, 280)
-
 class doubleConv(nn.Module):
     def __init__(self, input_size, output_size):
         super(doubleConv, self).__init__()
@@ -26,8 +18,6 @@ class doubleConv(nn.Module):
         x = self.doubleconv(x)
         return x
 
-
-
 class Unet(nn.Module):
     def __init__(self):
         super(Unet, self).__init__()
@@ -44,41 +34,8 @@ class Unet(nn.Module):
         self.outc = nn.Conv2d(64, 3, kernel_size=1)
         self.maxpool = nn.MaxPool2d(2, 2)
         self.unmawpool = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        '''     
-        self.encoder = nn.Sequential(
-            nn.ReLU(True),
-            nn.Conv2d(128, 128, kernel_size=3),
-            nn.ReLU(True),
-            nn.Maxpool2d(2, 2),
-            nn.Conv2d(128, 256, kernel_size=3),
-            nn.ReLU(True),
-            nn.Conv2d(256, 256, kernel_size=3),
-            nn.ReLU(True),
-            nn.Maxpool2d(2, 2),
-            Flatten(),
-            nn.Linear(280 * 280 * 16, 120),
-            nn.ReLU(True),
-            nn.Linear(120, 84),
-            nn.ReLU(True),
-            nn.Linear(84, 10))
 
-        self.decoder = nn.Sequential(
-            nn.Linear(10, 84),
-            nn.ReLU(True),
-            nn.Linear(84, 120),
-            nn.ReLU(True),
-            nn.Linear(120, 280 * 280 * 16),
-            UnFlatten(),
-            #nn.MaxUnpool2d(16, 16),
-            nn.ConvTranspose2d(16,6,kernel_size=5),
-            nn.ReLU(True),
-            #nn.MaxUnpool2d(16, 16),
-            nn.ConvTranspose2d(6,3,kernel_size=5),
-            nn.ReLU(True),
-            nn.Sigmoid())'''
-
-    def forward(self, x):
-        #start encoder
+    def encode(self, x):
         x1 = self.inc(x)
         x2 = self.maxpool(x1)
         x3 = self.down1(x2)
@@ -87,12 +44,11 @@ class Unet(nn.Module):
         x6 = self.maxpool(x5)
         x7 = self.down3(x6)
         x8 = self.maxpool(x7)
-
-        #latent space
         x9 = self.down4(x8)
+        return x9,x7,x5,x3,x1
 
-        #decoder
-        x10 = self.unmawpool(x9)
+    def decode(self,x,x7,x5,x3,x1):
+        x10 = self.unmawpool(x)
         x11 = self.up1(torch.cat([x10, x7], dim=1))
         x12 = self.unmawpool(x11)
         x13 = self.up2(torch.cat([x12, x5], dim=1))
@@ -100,7 +56,46 @@ class Unet(nn.Module):
         x15 = self.up3(torch.cat([x14, x3], dim=1))
         x16 = self.unmawpool(x15)
         x17 = self.up4(torch.cat([x16, x1], dim=1))
-        #x18 = self.unmawpool(x17)
+        # x18 = self.unmawpool(x17)
         x19 = self.outc(x17)
-
         return x19
+
+
+    def forward(self, x):
+        xlatent, x7,x5,x3,x1 = self.encode(x)
+        x = self.decode(xlatent, x7,x5,x3,x1)
+        return x
+
+
+    '''     
+    self.encoder = nn.Sequential(
+        nn.ReLU(True),
+        nn.Conv2d(128, 128, kernel_size=3),
+        nn.ReLU(True),
+        nn.Maxpool2d(2, 2),
+        nn.Conv2d(128, 256, kernel_size=3),
+        nn.ReLU(True),
+        nn.Conv2d(256, 256, kernel_size=3),
+        nn.ReLU(True),
+        nn.Maxpool2d(2, 2),
+        Flatten(),
+        nn.Linear(280 * 280 * 16, 120),
+        nn.ReLU(True),
+        nn.Linear(120, 84),
+        nn.ReLU(True),
+        nn.Linear(84, 10))
+
+    self.decoder = nn.Sequential(
+        nn.Linear(10, 84),
+        nn.ReLU(True),
+        nn.Linear(84, 120),
+        nn.ReLU(True),
+        nn.Linear(120, 280 * 280 * 16),
+        UnFlatten(),
+        #nn.MaxUnpool2d(16, 16),
+        nn.ConvTranspose2d(16,6,kernel_size=5),
+        nn.ReLU(True),
+        #nn.MaxUnpool2d(16, 16),
+        nn.ConvTranspose2d(6,3,kernel_size=5),
+        nn.ReLU(True),
+        nn.Sigmoid())'''
---
+++ src/VAE.py
@@ -0,0 +1,103 @@
+import torch.nn as nn
+import torch
+from torch.autograd import Variable
+from torch.nn import functional as F
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+class UnFlatten(nn.Module):
+    def forward(self, input, size=1024):
+        return input.view(input.size(0), 16, 280, 280)
+
+class doubleConv(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(doubleConv, self).__init__()
+
+        self.doubleconv = nn.Sequential(
+            nn.Conv2d(input_size, output_size, kernel_size=3, padding=1),
+            nn.BatchNorm2d(output_size),
+            nn.ReLU(True),
+            nn.Conv2d(output_size, output_size, kernel_size=3, padding=1),
+            nn.BatchNorm2d(output_size),
+            nn.ReLU(True),
+        )
+
+    def forward(self, x):
+        x = self.doubleconv(x)
+        return x
+
+
+
+class VUnet(nn.Module):
+    def __init__(self):
+        super(VUnet, self).__init__()
+
+        self.inc = doubleConv(3, 64)
+        self.down1 = doubleConv(64, 128)
+        self.down2 = doubleConv(128, 256)
+        self.down3 = doubleConv(256, 512)
+        self.down4 = doubleConv(512, 1024)
+        self.up1 = doubleConv(1024+512, 512)
+        self.up2 = doubleConv(512+256, 256)
+        self.up3 = doubleConv(256+128, 128)
+        self.up4 = doubleConv(128+64, 64)
+        self.outc = nn.Conv2d(64, 3, kernel_size=1)
+        self.maxpool = nn.MaxPool2d(2, 2)
+        self.unmawpool = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.flat = Flatten()
+        self.unflat = UnFlatten()
+        self.lin = nn.Linear(1024,1024)
+
+    def encode(self, x):
+        x1 = self.inc(x)
+        x2 = self.maxpool(x1)
+        x3 = self.down1(x2)
+        x4 = self.maxpool(x3)
+        x5 = self.down2(x4)
+        x6 = self.maxpool(x5)
+        x7 = self.down3(x6)
+        x8 = self.maxpool(x7)
+        x9 = self.down4(x8)
+        return x9,x7,x5,x3,x1
+
+    def decode(self,x,x7,x5,x3,x1):
+        x10 = self.unmawpool(x)
+        x11 = self.up1(torch.cat([x10, x7], dim=1))
+        x12 = self.unmawpool(x11)
+        x13 = self.up2(torch.cat([x12, x5], dim=1))
+        x14 = self.unmawpool(x13)
+        x15 = self.up3(torch.cat([x14, x3], dim=1))
+        x16 = self.unmawpool(x15)
+        x17 = self.up4(torch.cat([x16, x1], dim=1))
+        # x18 = self.unmawpool(x17)
+        x19 = self.outc(x17)
+
+    def reparametrize(self,x):
+        mu = self.lin(self.flat(x))
+        logvar = self.lin(self.flat(x))
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std,mu,logvar
+
+
+    def forward(self, x):
+        xlatent, x7,x5,x3,x1 = self.encode(self, x)
+        z,mu,logvar = self.reparameterize(xlatent)
+        x = self.decode(z, x7,x5,x3,x1)
+        return x, mu,logvar
+
+    def loss_function(recon_x, x, mu, logvar):
+        """
+        recon_x: generating images
+        x: origin images
+        mu: latent mean
+        logvar: latent log variance
+        """
+        BCE = nn.MSELoss(recon_x, x,size_average=False)  # mse loss
+        # loss = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
+        KLD = torch.sum(KLD_element).mul_(-0.5)
+        # KL divergence
+        return BCE + KLD
